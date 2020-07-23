e158d110e6e9e95ba16fcabfd80c75e9bdfd6435
Corey Martin <nefariousd@gmail.com>
2020-07-22 21:50:48 -0400

    Removed animejs

--- src/features/map/mapDisplay.tsx
+++ src/features/map/mapDisplay.tsx
@@ -27,11 +27,8 @@ import {
   Stack,
 } from '@chakra-ui/core';
 import { rng } from 'utilities/random.utilities';
-import { AnimeInstance } from 'animejs';
 
 export const MapDisplay = () => {
-  const animeRef = useRef<AnimeInstance>();
-
   const mapArea = useSelector(getCurrentMapArea);
 
   const dispatch = useDispatch();
@@ -43,17 +40,6 @@ export const MapDisplay = () => {
   const areaCells = useSelector(getCurrentMapCells);
 
   const translatedPlayerPos = useSelector(getTranslatedPlayerMapPos);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     if (animeRef.current) {
-  //       // animeRef.current.pause();
-  //       // or
-  //       // animeRef.current.reset();
-  //       animeRef.current.restart();
-  //     }
-  //   }, 1000);
-  // }, []);
 
   const renderMapCell = (cellType: AREA_CELL_TYPES, x: number, y: number) => {
     if (!AreaCellDisplayDefs[cellType]) {
@@ -97,60 +83,20 @@ export const MapDisplay = () => {
     for (let y = 0; y < areaCells[0].length; y++) {
       let row: JSX.Element[] = [];
       for (let x = 0; x < areaCells.length; x++) {
-        // let cellType = areaCells[x][y];
         row.push(renderMapCell(areaCells[x][y], x, y));
       }
       rows.push(row);
     }
   }
 
-  // const rows: Array<JSX.Element[]> = [];
-  // areaCells.map((mapRow, rowIdx) => {
-  //   const row: JSX.Element[] = [];
-  //   mapRow.map(cell => row.unshift(renderMapCell(cell)));
-  //   rows.push(row);
-  // });
-
   return (
     <Stack>
       <Box className="mapContainer">
-        {/*<TransitionGroup>*/}
-        {/*<Anime*/}
-        {/*  unmountOnExit={true}*/}
-        {/*  targets=".cellIcon"*/}
-        {/*  appear*/}
-        {/*  // onEntering={{ skewX: '-15deg'}}*/}
-        {/*  mountOnEnter={true}*/}
-        {/*  animeRef={animeRef}*/}
-        {/*  autoplay={false}*/}
-        {/*  loop={true}*/}
-        {/*  duration={1000}*/}
-        {/*  direction="alternate"*/}
-        {/*  // keyframes={[*/}
-        {/*  // { skewX: 15, duration: 250 },*/}
-        {/*  // { skewX: 0, duration: 250 },*/}
-        {/*  // ]}*/}
-        {/*  easing="easeInOutCubic"*/}
-        {/*  // keyframes={[*/}
-        {/*  //   // { skewX: '-15deg', duration: 0 },*/}
-        {/*  //   { skewX: '15deg', duration: 500 },*/}
-        {/*  //   { skewX: '-15deg', duration: 500 },*/}
-        {/*  // ]}*/}
-
-        {/*  // skewX={[*/}
-        {/*  //   { value: -15, duration: 500, delay: 0 },*/}
-        {/*  //   { value: 15, duration: 500, delay: 0 },*/}
-        {/*  // ]}*/}
-
-        {/*  skewX={['-15deg', '15deg']}*/}
-        {/*>*/}
-          {rows.map((row, rowIdx) => (
-            <div key={rowIdx} className="mapRow">
-              {row.map(content => content)}
-            </div>
-          ))}
-        {/*</Anime>*/}
-        {/*</TransitionGroup>*/}
+        {rows.map((row, rowIdx) => (
+          <div key={rowIdx} className="mapRow">
+            {row.map(content => content)}
+          </div>
+        ))}
       </Box>
 
       <Stack direction='row' justify='center' align='center'>
